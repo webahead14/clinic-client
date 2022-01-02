82a434ca6d37ffaae033d1d0a9f11d939c0367c5
“Zahi <zahi.moouess@gmail.com>
2022-01-02 12:55:40 +0200

    welcome page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,35 @@
 import style from './style.module.css'
-import { Button } from 'antd'
+// React Wavify
+import Wave from 'react-wavify'
+// star
 
 function Home(props) {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Button type="primary">Click me bro!</Button>
-        </div>
-    )
+  return (
+    <div>
+      <div className={style.title_p}>
+        <h1 className={style.title}>Hello!</h1>
+        <p className={style.par}>We’re glad to see you</p>
+      </div>
+      <div className={style.buttons}>
+        {/* redirct to login page */}
+        <form action="/login" className={style.center}>
+          <button className={style.loginButton}>Log-in</button>
+        </form>
+      </div>
+      {/* React Wavify */}
+      <Wave
+        className={style.waves}
+        fill="#DAEDFF"
+        paused={false}
+        options={{
+          height: 20,
+          amplitude: 30,
+          speed: 0.1,
+          points: 5,
+        }}
+      />
+    </div>
+  )
 }
 
 export default Home
