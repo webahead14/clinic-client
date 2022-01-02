88f24bd918c498c93aac4b85061602219c9cc69f
“Zahi <zahi.moouess@gmail.com>
2022-01-02 10:48:01 +0200

    finished

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,14 @@
 import style from "./style.module.css";
+// React Wavify
 import Wave from "react-wavify";
+
 function Login(props) {
   return (
+    // title
     <div className={style.background}>
       <h1 className={style.title}>Login</h1>
       <div className={style.inputs}>
+        {/* Id input */}
         <input
           type="text"
           className={style.usernameInput}
@@ -12,6 +16,7 @@ function Login(props) {
           required
         ></input>
         <br />
+        {/* password input */}
         <input
           type="password"
           className={style.passwordInput}
@@ -19,16 +24,22 @@ function Login(props) {
           required
         ></input>
         <br />
+        {/* login button */}
         <button className={style.loginButton}>Log-in</button>
+        {/* login by email */}
+        <a href="" className={style.loginchanger}>
+          Login By Email
+        </a>
       </div>
+      {/* React Wavify */}
       <Wave
         className={style.waves}
         fill="#DAEDFF"
-        paused={true}
+        paused={false}
         options={{
           height: 20,
           amplitude: 30,
-          speed: 0.2,
+          speed: 0.1,
           points: 5,
         }}
       />
