8bb844e74818be9d60719c53f4c832042fa6730a
Diabolica <scripter.Diabolica@gmail.com>
2022-01-04 15:21:04 +0200

    Changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,17 +7,10 @@ const data = require('./test.json')
 function Home(props) {
   return (
     <>
-      <div>
+      <div>const 
         <h1>Home</h1>
         <Button type="primary">Click me bro!</Button>
       </div>
-      <Matrix
-        instructions={data.questionnaire[0].instructions}
-        title={data.questionnaire[0].title}
-        columns={data.questionnaire[0].columns}
-        questions={data.questionnaire[0].questions}
-        answers={data.questionnaire[0].answers}
-      />
     </>
   )
 }
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,21 +1,56 @@
 import style from './style.module.css'
 import OpenText from '../../components/OpenText'
 import MultipleChoice from '../../components/MultipleChoice'
+import axios from 'axios'
 
-import data from '../../dataFromAPI/formattedQuestions.json'
+import Matrix from '../../components/Matrix'
+import MultipleChoice from '../../components/MultipleChoice'
+import OpenText from '../../components/OpenText'
 
-function Survey(props) {
+const { REACT_APP_API_URL } = process.env
+
+function Survey({ id, ...props }) {
+  const [data, setData] = React.useState([``])
+  const [currQuestion, setCurrQuestion] = React.useState(0)
+
+  const nextQuestion = () => {
+    setCurrQuestion((prev) =>
+      multipleQuestionParsedMatrix.questions.length - 1 > prev ? prev + 1 : prev
+    )
+  }
+
+  const prevQuestion = () => {
+    setCurrQuestion((prev) => (prev > 0 ? prev - 1 : prev))
+  }
+
+  axios.get(REACT_APP_API_URL + '/api/client/survey/' + id).then(setData)
   return (
     <div className={style.survey}>
-      {data.questionnaire.map((question, idx) => {
-        if (question.type === 'multiple_choice') {
-          return <MultipleChoice data={question} key={idx} />
-        }
-        if (question.type === 'open_text') {
-          return <OpenText data={question} key={idx} />
-        }
-        return null
-      })}
+      {data.map((question, idx) =>
+        question.type == 'matrix' ? (
+          <Matrix {...question} key={idx} />
+        ) : question.type == 'multiple_choice' ? (
+          <MultipleChoice data={question} key={idx} />
+        ) : question.type == 'open_text' ? (
+          <OpenText data={question} key={idx} />
+        ) : null
+      )}
+      <br />
+      {currQuestion > 0 ? (
+        <Button type="primary" onClick={prevQuestion}>
+          Prev Question
+        </Button>
+      ) : null}
+      {multipleQuestionParsedMatrix.questions.length - 1 > currQuestion ? (
+        <Button type="primary" onClick={nextQuestion}>
+          Next Question
+        </Button>
+      ) : null}
+      {multipleQuestionParsedMatrix.questions.length - 1 == currQuestion ? (
+        <Button type="primary" onClick={nextQuestion}>
+          Submit
+        </Button>
+      ) : null}
     </div>
   )
 }
