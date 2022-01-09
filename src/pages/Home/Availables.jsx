aa26b569dc80fde03a8e6116457f044e8e7fc646
Mario Saliba <mario.saliba98@gmail.com>
2022-01-09 23:46:54 +0200

    changes to the client's home

--- src/pages/Home/Availables.jsx
+++ src/pages/Home/Availables.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card } from 'antd'
-export default class Availables extends Component {
-  render() {
-    const { id, surveyName, lastDate, lastTime } = this.props.data
-    return (
-      <div>
-        <Card
-          title={surveyName}
-          extra={<a href="#">Start Questionnaire</a>}
-          style={{ width: 300 }}
-        >
-          <ul>
-            <li>Last Date:{lastDate}</li>
-            <li>Last Time:{lastTime}</li>
-          </ul>
-        </Card>
-      </div>
-    )
-  }
+export default function Availables(props) {
+  const { id, surveyName, lastDate, lastTime } = props.data
+
+  return (
+    <div>
+      <Card
+        title={surveyName}
+        extra={<Link to="#">Start Questionnaire</Link>}
+        style={{ width: 300 }}
+      >
+        <ul>
+          <li>Last Date:{lastDate}</li>
+          <li>Last Time:{lastTime}</li>
+        </ul>
+      </Card>
+    </div>
+  )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,52 +1,40 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Availables from './Availables'
 import style from './style.module.css'
 
-export default class AvailableSurveys extends Component {
-  constructor() {
-    super()
-    this.state = {
-      available: [
-        {
-          id: 1,
-          surveyName: 'PCL-5',
-          lastDate: '01-06',
-          lastTime: '00:00',
-        },
-        {
-          id: 2,
-          surveyName: 'GAD',
-          lastDate: '01-06',
-          lastTime: '00:00',
-        },
-        {
-          id: 3,
-          surveyName: 'PHQ',
-          lastDate: '01-06',
-          lastTime: '00:00',
-        },
-      ],
-    }
-  }
+export default function AvailableSurveys() {
+  const [available, setAvailable] = useState([
+    {
+      id: 1,
+      surveyName: 'PCL-5',
+      lastDate: '01-06',
+      lastTime: '00:00',
+    },
+    {
+      id: 2,
+      surveyName: 'GAD',
+      lastDate: '01-06',
+      lastTime: '00:00',
+    },
+    {
+      id: 3,
+      surveyName: 'PHQ',
+      lastDate: '01-06',
+      lastTime: '00:00',
+    },
+  ])
 
-  render() {
-    let list = []
-    for (var i = 0; i < this.state.available.length; i++) {
-      list.push(
-        <Availables key={i} data={this.state.available[i]}></Availables>
-      )
-    }
-
-    return (
-      <div>
-        <div className={style.logo}>
-          <p>Company Logo</p>
-        </div>
-        <h1 className={style.title}>Available Questionnaires</h1>
-        <div className={style.quest}>
-          <div>{list}</div>
-        </div>{' '}
+  return (
+    <div>
+      <div className={style.logo}>
+        <p>Company Logo</p>
       </div>
-    )
-  }
+      <h1 className={style.title}>Available Questionnaires</h1>
+      <div className={style.quest}>
+        <div>{available.map((obj, i) => (
+          <Availables key={i} data={obj} />
+        ))}</div>
+      </div>
+    </div>
+  )
 }
