a394379200406b47a0ddd39e4c56f91f347fb0ba
George Joubran <durd2001@gmail.com>
2022-01-16 14:12:01 +0200

    finished fetching avaliable surveys

--- src/pages/Home/Availables.jsx
+++ src/pages/Home/Availables.jsx
@@ -2,20 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from 'antd'
 export default function Availables(props) {
-  const { id, surveyName, lastDate, lastTime } = props.data
+  const { surveyId, surveyName } = props.data
 
   return (
     <div>
       <Card
         title={surveyName}
-        extra={<Link to="#">Start Questionnaire</Link>}
+        extra={<Link to="/survey/1">Start Questionnaire</Link>}
         style={{ width: 300 }}
-      >
-        <ul>
-          <li>Last Date:{lastDate}</li>
-          <li>Last Time:{lastTime}</li>
-        </ul>
-      </Card>
+      ></Card>
     </div>
   )
 }
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -7,7 +7,6 @@ import MultipleChoice from '../../components/MultipleChoice'
 import OpenText from '../../components/OpenText'
 import { Button } from 'antd'
 
-
 const { REACT_APP_API_URL } = process.env
 
 function Survey({ id = 1, ...props }) {
@@ -18,11 +17,12 @@ function Survey({ id = 1, ...props }) {
   const [totalQuestions, setTotalQuestions] = React.useState(0)
   const [currQuestionFromTotal, setCurrQuestionFromTotal] = React.useState(0)
 
-
   const nextQuestion = () => {
-    if (data[currQuestion].type === 'matrix' && currMatrixQuestion < data[currQuestion].questions.length - 1)
+    if (
+      data[currQuestion].type === 'matrix' &&
+      currMatrixQuestion < data[currQuestion].questions.length - 1
+    )
       setCurrMatrixQuestion((prev) => prev + 1)
-
     else
       setCurrQuestion((prev) => {
         if (data[prev + 1] && data[prev + 1].type == 'matrix')
@@ -41,7 +41,7 @@ function Survey({ id = 1, ...props }) {
       setCurrQuestion((prev) => {
         if (data[prev - 1] && data[prev - 1].type == 'matrix')
           setCurrMatrixQuestion(data[prev - 1].questions.length - 1)
-        return (prev > 0 ? prev - 1 : prev)
+        return prev > 0 ? prev - 1 : prev
       })
 
     setCurrQuestionFromTotal((prev) => prev - 1)
@@ -52,13 +52,20 @@ function Survey({ id = 1, ...props }) {
   }
 
   React.useEffect(() => {
-    axios.get(REACT_APP_API_URL + '/api/client/survey/' + id)
+    axios
+      .get(REACT_APP_API_URL + '/api/client/survey/' + id)
       .then((data) => data.data)
       .then(setData)
   }, [id])
 
   React.useEffect(() => {
-    setTotalQuestions(data.reduce((prev, question) => prev + (question.type === 'matrix' ? question.questions.length : 1), 0))
+    setTotalQuestions(
+      data.reduce(
+        (prev, question) =>
+          prev + (question.type === 'matrix' ? question.questions.length : 1),
+        0
+      )
+    )
   }, [data])
 
   if (!data.length) {
@@ -67,37 +74,42 @@ function Survey({ id = 1, ...props }) {
 
   return (
     <div className={style.survey}>
-      {data[currQuestion].type === 'matrix'
-        && <Matrix {...data[currQuestion]} setAnswers={saveAnswer} startingQuestionAnswers={questionAnswers[currQuestion]} currQuestion={currMatrixQuestion} />
-      }
-      {data[currQuestion].type === 'multiple_choice'
-        && <MultipleChoice data={data[currQuestion]} setAnswer={saveAnswer} answers={questionAnswers[currQuestion]} />
-      }
-      {data[currQuestion].type === 'open_text'
-        && <OpenText data={data[currQuestion]} setAnswer={saveAnswer} answers={questionAnswers[currQuestion]} />
-      }
+      {data[currQuestion].type === 'matrix' && (
+        <Matrix
+          {...data[currQuestion]}
+          setAnswers={saveAnswer}
+          startingQuestionAnswers={questionAnswers[currQuestion]}
+          currQuestion={currMatrixQuestion}
+        />
+      )}
+      {data[currQuestion].type === 'multiple_choice' && (
+        <MultipleChoice
+          data={data[currQuestion]}
+          setAnswer={saveAnswer}
+          answers={questionAnswers[currQuestion]}
+        />
+      )}
+      {data[currQuestion].type === 'open_text' && (
+        <OpenText
+          data={data[currQuestion]}
+          setAnswer={saveAnswer}
+          answers={questionAnswers[currQuestion]}
+        />
+      )}
       <br />
-      {currQuestionFromTotal > 0
-        ? (
-          <Button type='primary' onClick={prevQuestion}>
-            Prev Question
-          </Button>
-        )
-        : null}
-      {totalQuestions - 1 > currQuestionFromTotal
-        ? (
-          <Button type='primary' onClick={nextQuestion}>
-            Next Question
-          </Button>
-        )
-        : null}
-      {totalQuestions - 1 === currQuestionFromTotal
-        ? (
-          <Button type='primary'>
-            Submit
-          </Button>
-        )
-        : null}
+      {currQuestionFromTotal > 0 ? (
+        <Button type="primary" onClick={prevQuestion}>
+          Prev Question
+        </Button>
+      ) : null}
+      {totalQuestions - 1 > currQuestionFromTotal ? (
+        <Button type="primary" onClick={nextQuestion}>
+          Next Question
+        </Button>
+      ) : null}
+      {totalQuestions - 1 === currQuestionFromTotal ? (
+        <Button type="primary">Submit</Button>
+      ) : null}
     </div>
   )
 }
