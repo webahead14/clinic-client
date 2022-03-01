bf1fa8b78b6723f05f0d8ce7deb950805c103c63
hosenassadi <hosenassadi1991@gmail.com>
2022-03-01 14:23:25 +0200

    made home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,43 @@
 // REMOVE THIS LINE OF CODE, IT'S  JUST FOR SHOWING THE /home PAGE
-export default () => 'home page'
-
-
-// WRITE YOUR CODE BELOW THIS LINE
+import style from './style.module.css'
+import './style.css'
+import React from 'react'
+import Logo from './logo.png'
+import { Card, Space } from 'antd'
+function Home() {
+  // WRITE YOUR CODE BELOW THIS LINE
+  return (
+    <div>
+      <div className={style.header}>
+        <img src={Logo}></img>
+      </div>
+      <div className={style.body}>
+        <div className={style.title}>
+          <h1>Available Questionnaires</h1>
+        </div>
+        <div className={style.cards}>
+          <Space size="small" direction="vertical">
+            <Card title="GAD" size="small" style={{ width: '90%' }}>
+              <a href="https://wa14-clinic-api.herokuapp.com/api/client/surveys/1">
+                <h3 className={style.start} className={style.start}>
+                  Start Questionnaire
+                </h3>
+              </a>
+            </Card>
+            <Card title="PGI-S" size="small" style={{ width: '90%' }}>
+              <a href="https://wa14-clinic-api.herokuapp.com/api/client/surveys/2">
+                <h3 className={style.start}> Start Questionnaire</h3>
+              </a>
+            </Card>
+            <Card title="PCL-5" size="small" style={{ width: '90%' }}>
+              <a href="https://wa14-clinic-api.herokuapp.com/api/client/surveys/3">
+                <h3 className={style.start}> Start Questionaire</h3>
+              </a>
+            </Card>
+          </Space>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Home
